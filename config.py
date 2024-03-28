df3d9ed79567fdf23bfcb3f2a89c8c739b99c65c
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-03-28 17:52:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1002077252225  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1001928835339  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [6301693754]
