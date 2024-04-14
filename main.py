88f0b09282dfe48c90acfe90ed929c712f831a36
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-04-14 09:32:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,14 +118,7 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
-    if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again 5.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
-    if not check_if:
-        return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
-        )
+    
     is_spam = db.get(m.sender_id)
     if is_spam and m.sender_id not in [1317173146]:
         return await m.reply("You are spamming. Please wait a 1 minute and try again.")
