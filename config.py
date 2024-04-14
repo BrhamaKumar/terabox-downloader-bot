6ba52aa60beabef207b16563ffd18949986f46c5
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-04-14 08:59:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 API_ID = 20810825  # api id
 API_HASH = "707e67f53b4593a3e9b6b424311f84d0"  # api hash
 
-BOT_TOKEN = "6382802673:AAHqgHD_ypCGmjjlhL-bNUluy-Sm0kjn4yY"  # bot token
+BOT_TOKEN = "6870285930:AAHkOuGTAWcgL5C7QLU_pa5-K0ohFmfffTQ"  # bot token
 
 
 ## REDIS
-HOST = "redis-12162.c212.ap-south-1-1.ec2.cloud.redislabs.com:12162"  # redis host uri
+HOST = "redis-12162.c212.ap-south-1-1.ec2.cloud.redislabs.com"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = "38eH7Ch6GjhqK9XTSTRma4KVUnWe4yMz"  # redis password
 
-PRIVATE_CHAT_ID = -1001928835339  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002077252225  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [6301693754]
