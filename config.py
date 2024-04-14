199399e0ca31cd9379ece63a11eeef76fbc86536
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-04-14 09:48:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ BOT_TOKEN = "6870285930:AAHkOuGTAWcgL5C7QLU_pa5-K0ohFmfffTQ"  # bot token
 
 
 ## REDIS
-HOST = "redis-12162.c212.ap-south-1-1.ec2.cloud.redislabs.com"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = "38eH7Ch6GjhqK9XTSTRma4KVUnWe4yMz"  # redis password
+HOST = "redis-11411.c253.us-central1-1.gce.cloud.redislabs.com"  # redis host uri
+PORT = 11411  # redis port
+PASSWORD = "PXPSK742MCOfMCSN0g0kPmrjquC3p3aD"  # redis password
 
 PRIVATE_CHAT_ID = -1002077252225  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
